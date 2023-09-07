eb092817fabce570f67cec43d7169de0d5e2368b
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-09-07 18:45:35 -0400

    go

--- main.py
+++ main.py
@@ -13,6 +13,22 @@ with st.expander("Step 1: Add personal info here"):
     personal_statement = st.text_area("Personal statement", placeholder="I'm a 22 year old guy who likes AI, crypto, communities, and cats.")
     # conditional_statements = st.text_area("")
 
+def GenerateReport():
+    system_prompt = f"""
+        You are a professional profiler who wants to describe a person for a lawyer.
+        Describe the kind of values and interests this person might have.
+        You have to describe this person for a jury. The jury will make a decision for the person.
+        """
+    result = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": system_prompt}, # Bot is representative
+            {"role": "user", "content": person}
+            ]
+        )
+    report = result['choices'][0]['message']['content']
+    return report
+
 # STORE IN DATABASE
 
 
